ee263e6ed42c3216bae53b28a4e7e33c1e81c244
lin-nguyen <linh1612999@gmail.com>
2022-09-03 21:32:32 +0700

    vn-translation: first commit translation

---
+++ src/culture/vn.js
@@ -0,0 +1,166 @@
+(function (translator) {
+	translator.translations["vn"] = {
+		// javascript alerts or messages
+		"testneteditionactivated": "Phiên bản TESTNET đã được kích hoạt",
+		"paperlabelbitcoinaddress": "Địa chỉ ví bitcoin",
+		"paperlabelprivatekey": "Khoá riêng tư:",
+		"paperlabelencryptedkey": "",
+		"bulkgeneratingaddresses": "Đang tạo địa chỉ ví... ",
+		"brainalertpassphrasetooshort": "Bạn đã nhập mật khẩu quá ngắn.\n\n",
+		"brainalertpassphrasewarning": "Cảnh báo: Việc nhập mật khẩu đủ mạnh là vô cùng quan trọng để tránh các hệ thống vét cạn đoán được mật khẩu và lấy cắp mất bitcoin của bạn.",
+		"brainalertpassphrasedoesnotmatch": "Mật khẩu không khớp với mật khẩu xác nhận.",
+		"detailalertnotvalidprivatekey": "Bạn đã nhập khoá riêng tư không hợp lệ",
+		"detailconfirmsha256": "Bạn đã nhập khoá riêng tư không hợp lệ!\n\nBanj có muốn tạo khoá riêng tư sử dụng giải thuật SHA256 không?\n\nCảnh báo: Việc nhập mật khẩu đủ mạnh là vô cùng quan trọng để tránh các hệ thống vét cạn đoán được mật khẩu và lấy cắp mất bitcoin của bạn.",
+		"bip38alertincorrectpassphrase": "Mật khẩu không chính xác cho khoá cá nhân.",
+		"bip38alertpassphraserequired": "Khoá BIP38 bắt buộc cần mật khẩu.",
+		"vanityinvalidinputcouldnotcombinekeys": "",
+		"vanityalertinvalidinputpublickeysmatch": "",
+		"vanityalertinvalidinputcannotmultiple": "",
+		"vanityprivatekeyonlyavailable": "",
+		"vanityalertinvalidinputprivatekeysmatch": "",
+
+		// header and menu html
+		"tagline": "",
+		"generatelabelbitcoinaddress": "",
+		"generatelabelmovemouse": "",
+		"generatelabelkeypress": "",
+		"singlewallet": "",
+		"paperwallet": "Ví giấy",
+		"bulkwallet": "",
+		"brainwallet": "",
+		"vanitywallet": "",
+		"splitwallet": "", //TODO: please translate
+		"detailwallet": "Chi tiết về ví",
+
+		// footer html
+		"footerlabeldonations": "",
+		"footerlabeltranslatedby": "",
+		"footerlabelpgp": "",
+		"footerlabelversion": "",
+		"footerlabelgithub": "",
+		"footerlabelgithubzip": "",
+		"footerlabelsig": "",
+		"footerlabelcopyright1": "",
+		"footerlabelcopyright2": "",
+		"footerlabelnowarranty": "",
+
+		// status html
+		"statuslabelcryptogood": "", //TODO: please translate
+		"statuslabelcryptogood1": "", //TODO: please translate
+		"statusokcryptogood": "", //TODO: please translate
+		"statuslabelcryptobad": "", //TODO: please translate
+		"statuslabelcryptobad1": "",
+		"statusokcryptobad": "", //TODO: please translate
+		"statuslabelunittestsgood": "", //TODO: please translate
+		"statuslabelunittestsgood1": "", //TODO: please translate
+		"statusokunittestsgood": "", //TODO: please translate
+		"statuslabelunittestsbad": "", //TODO: please translate
+		"statuslabelunittestsbad1": "", //TODO: please translate
+		"statusokunittestsbad": "", //TODO: please translate
+		"statuslabelprotocolgood": "", //TODO: please translate
+		"statuslabelprotocolgood1": "", //TODO: please translate
+		"statusokprotocolgood": "", //TODO: please translate
+		"statuslabelprotocolbad": "", //TODO: please translate
+		"statuslabelprotocolbad1": "", //TODO: please translate
+		"statuslabelprotocolbad2": "", //TODO: please translate
+		"statuslabelprotocolbad3": "", //TODO: please translate
+		"statusokprotocolbad": "", //TODO: please translate
+		"statuslabelkeypool1": "", //TODO: please translate
+		"statuskeypoolrefresh": "", //TODO: please translate
+		"statusokkeypool": "", //TODO: please translate
+
+		// single wallet html
+		"newaddress": "",
+		"singleprint": "",
+		"singlelabelbitcoinaddress": "",
+		"singlelabelprivatekey": "",
+		"singletip1": "",
+		"singletip2": "",
+		"singletip3": "",
+		"singletip4": "",
+		"singletip5": "",
+		"singleshare": "",
+		"singlesecret": "",
+
+		// paper wallet html
+		"paperlabelhideart": "",
+		"paperlabeladdressesperpage": "",
+		"paperlabeladdressestogenerate": "",
+		"papergenerate": "",
+		"paperprint": "",
+		"paperlabelBIPpassphrase": "",
+		"paperlabelencrypt": "",
+
+		// bulk wallet html
+		"bulklabelstartindex": "",
+		"bulklabelrowstogenerate": "",
+		"bulklabelcompressed": "",
+		"bulkgenerate": "",
+		"bulkprint": "",
+		"bulklabelcsv": "",
+		"bulklabelformat": "",
+		"bulklabelq1": "",
+		"bulka1": "",
+		"bulklabelq2": "",
+		"bulklabela2li1": "",
+		"bulklabela2li2": "",
+		"bulklabela2li3": "",
+		"bulklabela2li4": "",
+		"bulklabela2li5": "",
+
+		// brain wallet html
+		"brainlabelenterpassphrase": "",
+		"brainlabelshow": "",
+		"brainprint": "",
+		"brainlabelconfirm": "",
+		"brainview": "",
+		"brainalgorithm": "",
+		"brainlabelbitcoinaddress": "",
+		"brainlabelprivatekey": "",
+
+		// vanity wallet html
+		"vanitylabelstep1": "",
+		"vanitynewkeypair": "",
+		"vanitylabelstep1publickey": "",
+		"vanitylabelstep1pubnotes": "",
+		"vanitylabelstep1privatekey": "",
+		"vanitylabelstep1privnotes": "",
+		"vanitylabelstep2calculateyourvanitywallet": "",
+		"vanitylabelenteryourpart": "",
+		"vanitylabelenteryourpoolpart": "",
+		"vanitylabelnote1": "",
+		"vanitylabelnote2": "",
+		"vanitylabelradioadd": "",
+		"vanitylabelradiomultiply": "",
+		"vanitycalc": "",
+		"vanitylabelbitcoinaddress": "",
+		"vanitylabelnotesbitcoinaddress": "",
+		"vanitylabelpublickeyhex": "",
+		"vanitylabelnotespublickeyhex": "",
+		"vanitylabelprivatekey": "",
+		"vanitylabelnotesprivatekey": "",
+
+		// detail wallet html
+		"detaillabelenterprivatekey": "",
+		"detailkeyformats": "",
+		"detailview": "",
+		"detailprint": "",
+		"detaillabelnote1": "",
+		"detaillabelnote2": "",
+		"detaillabelbitcoinaddress": "",
+		"detaillabelbitcoinaddresscomp": "",
+		"detaillabelpublickey": "",
+		"detaillabelpublickeycomp": "",
+		"detaillabelprivwif": "",
+		"detaillabelprivwifcomp": "",
+		"detailcompwifprefix": "",
+		"detaillabelprivhex": "",
+		"detaillabelprivb64": "",
+		"detaillabelprivmini": "",
+		"detaillabelpassphrase": "",
+		"detailbip38decryptbutton": "",
+		"detailbip38encryptbutton": "", //TODO: please translate
+		"detaillabelq1": "",
+		"detaila1": ""
+	};
+})(ninja.translator);
